1f2c1eb6975f47ca4d34647efc5e54b4a102869d
jenna.nicodemus <jenna.nicodemus@cs-dolphin.centre.edu>
2024-10-01 08:20:13 -0400

    tests

---
+++ src/test/java/TestStack.java
@@ -0,0 +1,55 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestStack
+{
+
+	MyStack stack;
+	String stringA;
+	String stringB;
+	String stringC;
+	
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		MyStack stack = new MyStack();
+		stringA = "Hi";
+		stringB = "Hello";
+		stringC = "Greetings";
+	}
+
+	@Test
+	void testStackUnderFlow()
+	{
+		assertThrows(StackUnderFlowException.class, () -> {
+			stack.pop();
+		});
+	}
+	
+	@Test
+	void testStack()
+	{
+		stack.push(stringA);
+		assertEquals(stringA, stack.top());
+	
+		stack.push(stringB);
+		stack.push(stringC);
+		
+		assertEquals(stringA, stack.pop());
+		
+		assertEquals(stringB, stack.top());
+		assertEquals(stringB, stack.pop());
+		
+		assertEquals(stringC, stack.top());
+		assertEquals(stringC, stack.pop());
+		
+		assertEquals(true, stack.isEmpty());
+		
+		assertThrows(StackUnderFlowException.class, () -> {
+			stack.pop();
+		});
+	}
+
+}
